f4bec4fcb4ddf440d08be923ea6e958b332dd3d3
Veraprot <vera.protopopova@gmail.com>
2019-01-08 13:28:05 -0500

    build basic dnd for cards

--- src/components/game/GameBoard.js
+++ src/components/game/GameBoard.js
@@ -1,24 +1,62 @@
-import React from 'react'
-import {connect} from 'react-redux'
+import React, { Component } from 'react';
+import CardDeck from './CardDeck';
+import Timeline from './Timeline';
 
-class GameBoard extends React.Component {
-  constructor(props) {
-    super(props)
+import HTML5Backend from 'react-dnd-html5-backend'
+import { DragDropContext } from 'react-dnd'
+const update = require('immutability-helper');
+
+class GameBoard extends Component {
+  state = {
+    items: [
+      { id: 1, name: 'Item 1' },
+      { id: 2, name: 'Item 2' },
+      { id: 3, name: 'Item 3' },
+      { id: 4, name: 'Item 4' },
+    ],
+    targetItems: [],
+  }
+
+  deleteItem = id => {
+    let dragItem = this.state.items.find(item => item.id == id)
+    this.setState(prevState => {
+      return {
+        items: prevState.items.filter(item => item.id !== id),
+        targetItems: [...prevState.targetItems, dragItem]
+      }
+    })
+  }
+
+  moveItem = (dragIndex, hoverIndex) => {
+    const { targetItems } = this.state
+    const dragItem = targetItems[dragIndex]
+
+    this.setState(
+      update(this.state, {
+        targetItems: {
+          $splice: [[dragIndex, 1], [hoverIndex, 0, dragItem]], 
+        }
+      })
+    )
   }
 
   render() {
     return (
-      <h1>game board</h1>
-    )
+      <>
+      <div className="item-container">
+        {this.state.items.map((item, index) => (
+          <CardDeck 
+            key={item.id}
+            item={item}
+            handleDrop={(id) => this.deleteItem(id)}
+              />
+        ))}
+      </div>
+
+      <Timeline items={this.state.targetItems} moveItem={this.moveItem}/>
+      </>
+    );
   }
 }
 
-
-const mapStateToProps = state => ({
-  game: state.game
-});
-
-
-export default connect(mapStateToProps, { })(
-  (GameBoard)
-);
+export default DragDropContext(HTML5Backend)(GameBoard);
