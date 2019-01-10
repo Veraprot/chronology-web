f031e0a8bf353f6398b0341c407b53f8afd00099
Veraprot <vera.protopopova@gmail.com>
2019-01-10 14:30:54 -0500

    add game answer logic

--- src/components/game/GameBoard.js
+++ src/components/game/GameBoard.js
@@ -8,45 +8,46 @@ import {moveCard, updateCard } from '../../actions/gameActions';
 const checkAnswer = (activeCard, destination, droppableDestination ) => {
   if( droppableDestination.index == 0 ) {
     let dateAfter = destination[droppableDestination.index].date
-    console.log('first index', dateAfter)
     return activeCard.date < dateAfter
   } else 
     if(droppableDestination.index == destination.length) {
     let cardBefore = destination[droppableDestination.index - 1].date
-    console.log('last index', cardBefore)
     return activeCard.date > cardBefore
   } else {
     let cardBefore = destination[droppableDestination.index - 1].date
     let cardAfter = destination[droppableDestination.index].date
-    console.log('middle index', cardBefore, cardAfter)
     return activeCard.date > cardBefore && activeCard.date < cardAfter
   }
 }
-
-const move = (source, destination, droppableSource, droppableDestination) => {
-  const sourceClone = Array.from(source);
-  const destClone = Array.from(destination);
-
-  const [activeCard] = sourceClone.splice(droppableSource.index, 1); 
-
-  const answer = checkAnswer(activeCard, destination, droppableDestination);
-
-  const result = {};
-  // if(answer) {
-    destClone.splice(droppableDestination.index, 0, activeCard);
-    result[droppableSource.droppableId] = sourceClone;
-    result[droppableDestination.droppableId] = destClone;
-  // } else {
-  //   result[droppableSource.droppableId] = activeCard;
-  //   result[droppableDestination.droppableId] = destClone;
-  // }
-  return result;
-};
 
 class GameBoard extends Component {
   id2List = {
     activeCard: 'activeCard',
     answeredCards: 'answeredCards'
+  };
+
+  move = (source, destination, droppableSource, droppableDestination) => {
+    const sourceClone = Array.from(source);
+    const destClone = Array.from(destination);
+  
+    const [activeCard] = sourceClone.splice(droppableSource.index, 1); 
+    console.log('active card', activeCard)
+    const answer = checkAnswer(activeCard, destination, droppableDestination);
+    console.log(answer)
+  
+    const result = {};
+    if(answer) {
+      destClone.splice(droppableDestination.index, 0, activeCard);
+      result[droppableSource.droppableId] = sourceClone;
+      result[droppableDestination.droppableId] = destClone;
+      this.props.moveCard(result.answeredCards)
+      this.props.updateCard(this.props.game.cards);
+    } else {
+      result[droppableSource.droppableId] = activeCard;
+      result[droppableDestination.droppableId] = destClone;
+    }
+    console.log(result)
+    return result;
   };
 
   getList = id => this.props.game[this.id2List[id]];
@@ -58,15 +59,12 @@ class GameBoard extends Component {
     }
 
     if (source.droppableId !== destination.droppableId) { 
-      const result = move(
+      const result = this.move(
         this.getList(source.droppableId),
         this.getList(destination.droppableId),
         source,
         destination
       );
-
-      this.props.moveCard(result.answeredCards);
-      this.props.updateCard(this.props.game.cards)
     }
   };
 
